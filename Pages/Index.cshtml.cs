8eacbce0875d4de06c990dd016abe11eda0e08c6
Fernando Clemente <fe-clemente@hotmail.com>
2024-07-31 16:50:19 -0300

    V1

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+
+namespace MyRazorApp.Pages
+{
+    public class IndexModel : PageModel // Classe que herda PageModel
+    {
+        public List<Category> Categories { get; set; } = new();
+        
+        public async Task OnGet()
+        {
+            await Task.Delay(5000);
+            for (var i = 0; i < 100; i++)
+            {
+                Categories.Add(
+                    new Category(i, 
+                    $"Categoria {i}", 
+                    i * 18.95M));
+            }
+        }
+    }
+
+}
+
+    public record Category(int Id, string Title, decimal Price); // Categoria armazenada em lista acima
