df293ab7b2913494c6a1fa368a028396f58521b6
Fernando Clemente <fe-clemente@hotmail.com>
2024-07-31 16:53:57 -0300

    v2

--- obj/Debug/net6.0/MyRazorApp.AssemblyInfo.cs
+++ obj/Debug/net6.0/MyRazorApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyRazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8eacbce0875d4de06c990dd016abe11eda0e08c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyRazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyRazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
